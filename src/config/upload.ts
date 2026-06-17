7682ad42f5f0e939e869a641bfdee3d005359eda
Juan Mafra <juan.carlos2113@hotmail.com>
2026-06-17 13:59:06 -0300

    fix: upload file, add process.cwd

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,10 +1,18 @@
 import multer from "multer";
 import path from "node:path";
 import crypto from "node:crypto";
+import fs from "node:fs";
 
-const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
+const TMP_FOLDER = path.resolve(process.cwd(), "tmp");
 const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, "uploads");
 
+if (!fs.existsSync(TMP_FOLDER)) {
+  fs.mkdirSync(TMP_FOLDER, { recursive: true });
+}
+
+if (!fs.existsSync(UPLOADS_FOLDER)) {
+  fs.mkdirSync(UPLOADS_FOLDER, { recursive: true });
+}
 const MAX_SIZE = 3;
 const MAX_FILE_SIZE = 1024 * 1024 * MAX_SIZE;
 const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png"];
